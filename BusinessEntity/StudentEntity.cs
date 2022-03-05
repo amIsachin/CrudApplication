17130f1a6c6ad32442bc2b9a670f390d1a58d140
amIsachin <amIsachin@hotmail.com>
2022-03-05 14:27:33 +0530

    Initialize Entity And Basic Setup

--- BusinessEntity/StudentEntity.cs
+++ BusinessEntity/StudentEntity.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BusinessEntity
+﻿namespace BusinessEntity
 {
-    internal class StudentEntity
+    public sealed class StudentEntity : BaseEntity
     {
     }
 }
