153a6eeb403d3819da40f1d3ae77f79ff461df69
amIsachin <amIsachin@hotmail.com>
2022-03-06 14:05:18 +0530

    Created Student Property

--- BusinessEntity/StudentEntity.cs
+++ BusinessEntity/StudentEntity.cs
@@ -1,19 +1,17 @@
-﻿namespace BusinessEntity
+﻿using System;
+
+namespace BusinessEntity
 {
     public sealed class StudentEntity : BaseEntity
     {
-        public void Print()
-        {
-            base.ID = 10;
-        }
-    }
-
-    public class ABC
-    {
-        public void Print()
-        {
-            BaseEntity b = new BaseEntity();
-            b.ID = 110;
-        }
+        public int RollNumber { get; set; }
+        public string Name { get; set; }
+        public string Standard { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+        public double Fees { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+        public DateTime AdmissionSession { get; set; }
     }
 }
