a87d8f8ca04b85aadc6191a40aa7c2df1989a4b1
amIsachin <amIsachin@hotmail.com>
2022-03-06 13:23:38 +0530

    Test

--- BusinessEntity/StudentEntity.cs
+++ BusinessEntity/StudentEntity.cs
@@ -2,5 +2,18 @@
 {
     public sealed class StudentEntity : BaseEntity
     {
+        public void Print()
+        {
+            base.ID = 10;
+        }
+    }
+
+    public class ABC
+    {
+        public void Print()
+        {
+            BaseEntity b = new BaseEntity();
+            b.ID = 110;
+        }
     }
 }
