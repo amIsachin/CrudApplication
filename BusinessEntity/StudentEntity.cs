15e1d8c9d5b85d4dd6b406f6d9cf50ea0b8b4b1f
amIsachin <amIsachin@hotmail.com>
2022-03-07 13:26:33 +0530

    Add Connection String And Change Data Type Two Properties  In Student Entity

--- BusinessEntity/StudentEntity.cs
+++ BusinessEntity/StudentEntity.cs
@@ -6,10 +6,10 @@ namespace BusinessEntity
     {
         public int RollNumber { get; set; }
         public string Name { get; set; }
-        public string Standard { get; set; }
+        public string Class { get; set; }
         public string Gender { get; set; }
         public int Age { get; set; }
-        public double Fees { get; set; }
+        public decimal Fees { get; set; }
         public string City { get; set; }
         public string Address { get; set; }
         public DateTime AdmissionSession { get; set; }
