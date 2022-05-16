ba9093150be53aea61d9574de1bd41b89831fed8
amIsachin <amIsachin@hotmail.com>
2022-05-16 13:50:37 +0530

    Apply string Length data annotation and add one route in student Controller

--- ViewModels/StudentEntityViewModels.cs
+++ ViewModels/StudentEntityViewModels.cs
@@ -12,18 +12,23 @@ namespace ViewModels
         public int RollNumber { get; set; }
 
         [Required]
+        [StringLength(50, MinimumLength = 2)]
         public string Name { get; set; }
         [Required]
+        [StringLength(20, MinimumLength = 1)]
         public string Class { get; set; }
         [Required]
+        [StringLength(10, MinimumLength = 1)]
         public string Gender { get; set; }
         [Required]
         public int Age { get; set; }
         [Required]
         public decimal Fees { get; set; }
         [Required]
+        [StringLength(50, MinimumLength = 2)]
         public string City { get; set; }
         [Required]
+        [StringLength(100, MinimumLength = 2)]
         public string Address { get; set; }
         [Required]
         public DateTime AdmissionSession { get; set; }
