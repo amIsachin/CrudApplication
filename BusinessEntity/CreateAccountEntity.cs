9a6915568dee8d04ed0f48086e71e663de0ef765
amIsachin <amIsachin@hotmail.com>
2022-07-18 14:49:30 +0530

    Create Create Account Entity And completed createAcoount view work done

---
+++ BusinessEntity/CreateAccountEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace BusinessEntity
+{
+    internal class CreateAccountEntity : BaseEntity
+    {
+        public string UserName { get; set; }
+        public string Emial { get; set; }
+        public string Number { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
--- Crud.Web/Controllers/AuthenticationController.cs
+++ Crud.Web/Controllers/AuthenticationController.cs
@@ -4,9 +4,12 @@ namespace Crud.Web.Controllers
 {
     public class AuthenticationController : Controller
     {
+        [HttpGet]
         public ActionResult CreateAccount()
         {
             return View();
         }
+
+
     }
 }
