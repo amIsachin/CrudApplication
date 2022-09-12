0415648d6233d134052ae436770727256f6d4317
amIsachin <amIsachin@hotmail.com>
2022-09-12 14:23:00 +0530

    Image upload functionality but not done  in university controller

---
+++ Crud.Web/Controllers/SharedController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Crud.Web.Controllers
+{
+    public class SharedController : Controller
+    {
+        [HttpPost]
+        public ActionResult UploadImage(string imageUrl)
+        {
+            return View();
+        }
+    }
+}
